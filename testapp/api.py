ebd38a31c401f897af7ce50f8f6722ad9762fb60
jythi <jyothireddy6696@gmail.com>
2020-07-08 18:07:22 +0530

    First Commit

---
+++ testapp/api.py
@@ -0,0 +1,11 @@
+# from testapp.models import roles
+# from rest_framework import viewsets,permissions
+# from .serializers import rolesSerializer
+
+
+# class rolesViewSet(viewsets.ModelViewSet):
+#     queryset=roles.objects.all()
+#     permission_classes=[
+#         permissions.AllowAny
+#     ]
+#     serializer_class=rolesSerializer
